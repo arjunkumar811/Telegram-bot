399dc017ddb6dd8825406a8a8808e6e30d1a4b7e
arjunkumar811 <kumararjun26401@gmail.com>
2026-06-17 17:15:57 +0530

    Added Code

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { Telegraf } from "telegraf";
+import { config } from "dotenv";
+config();
+
+const token = process.env.BOT_TOKEN;
+if(!token) throw new Error("Bot Token not found .env");
+
+const bot = new Telegraf(token);
+
+bot.start((ctx) => {
+    
+})
