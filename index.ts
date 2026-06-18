544c8f8dca93c7f1b3f5b72c6fcd3a64ae9f85e1
arjunkumar811 <kumararjun26401@gmail.com>
2026-06-18 09:21:42 +0530

    Welcome to Bot

--- index.ts
+++ index.ts
@@ -4,9 +4,19 @@ config();
 
 const token = process.env.BOT_TOKEN;
 if(!token) throw new Error("Bot Token not found .env");
+console.log("TOKEN =", process.env.BOT_TOKEN);
 
 const bot = new Telegraf(token);
 
-bot.start((ctx) => {
+bot.start(async(ctx) => {
+    const userid = ctx.from?.id;
+    if(!userid) return;
+
+    let welcomeMessage = `Welcome to solona telegram bot wallet build by Arjun❤️`;
+    return ctx.reply(welcomeMessage);
+
     
 })
+
+
+await bot.launch();
