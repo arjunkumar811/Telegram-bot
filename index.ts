009003022ed5e29d683780a156c9de361a78cec8
arjunkumar811 <kumararjun26401@gmail.com>
2026-07-18 13:40:14 +0530

    action

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Telegraf } from "telegraf";
+import { Markup, Telegraf } from "telegraf";
 import { config } from "dotenv";
 config();
 
@@ -8,15 +8,33 @@ console.log("TOKEN =", process.env.BOT_TOKEN);
 
 const bot = new Telegraf(token);
 
+const keyboard = Markup.inlineKeyboard([
+    [
+        Markup.button.callback(' 🔑 Generate new wallets', 'generate_wallet'),
+        Markup.button.callback(' 👁️ View Address ', 'view_address'),
+    ],
+])
+
 bot.start(async(ctx) => {
     const userid = ctx.from?.id;
     if(!userid) return;
 
     let welcomeMessage = `Welcome to solona telegram bot wallet build by Arjun❤️`;
-    return ctx.reply(welcomeMessage);
+    return ctx.reply(welcomeMessage, {
+        parse_mode: "Markdown",
+        ...keyboard
+    });   
+});
 
-    
+
+bot.action("generate_wallet", (ctx) => {
+    ctx.answerCbQuery("Generating Wallet....");
 })
 
+
+
+bot.action("view_address", (ctx) => {
+    ctx.answerCbQuery("Showing your address");
+})
 
 await bot.launch();
